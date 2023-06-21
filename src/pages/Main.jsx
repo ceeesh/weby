107e73219e579bfd59d84df9e2f44b2c29fe4413
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-06-21 18:50:33 +0800

    headers bug fixed

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useContext } from 'react'
 import Intro from '../components/Intro'
 import Process from '../components/Process'
 import Testimonial from '../components/Testimonial'
 import Header from '../layout/Header'
+import { ClientContext } from '../contexts/ClientContext'
 
 const Main = () => {
+  const { updateLoginInfo, updateProjects, updatePicture } = useContext(ClientContext)
+  
+  useEffect(() => {
+    updateLoginInfo(null);
+    updateProjects(null);
+    updatePicture(null)
+  }, [])
+
   return (
     <>
       <Header />
